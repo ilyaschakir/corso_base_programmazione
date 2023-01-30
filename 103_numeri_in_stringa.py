5d7f1bf52db00fee1beb08ac4112173ac2206e8f
ilyaschakir <115661435+ilyaschakir@users.noreply.github.com>
2023-01-30 19:53:43 +0100

    Add files via upload

---
+++ 103_numeri_in_stringa.py
@@ -0,0 +1,21 @@
+i = 0
+counter = 0
+c = 0
+somma = 0
+stringa = input("Inserisci una stringa numerica: ")
+counter = len(stringa)
+while i < counter:
+    x = stringa[i]
+    if(int(c)):
+        i = i + 1
+    else:
+        print("Inserisci solo numeri")
+        break
+i = 0
+while i < counter:
+    c = stringa[i]
+    somma = somma + int(c)
+    i = i + 1
+print(somma)
+
+
